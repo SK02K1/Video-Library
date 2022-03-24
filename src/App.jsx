d757613cd95340b51644efbf2c445f85ba5395f0
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2022-03-24 16:48:17 +0530

    add: homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Home } from './pages';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Focus-TV</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
