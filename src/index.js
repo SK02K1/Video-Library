1ed8fc90391cb3de71e717d490f667243fc64953
SK02K1 <ksourabh458@gmail.com>
2022-03-25 19:30:25 +0530

    style: add secondary typeface

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { makeServer } from './server';
+import './fonts/NaftaLight.woff';
+import './index.css';
 
 // Call make Server
 makeServer();
