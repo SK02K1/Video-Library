5d32fa6ac8ef56145b167d19c758c1c8860fddd7
SK02K1 <ksourabh458@gmail.com>
2022-03-28 21:30:05 +0530

    add: profile controls

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,7 +1,20 @@
+import { profileControls } from '../../utils';
+import { Link } from 'react-router-dom';
+import './Profile.css';
+
 export const Profile = () => {
   return (
     <div className='content'>
-      <h1 className='text-xl text-center m-md-tb'>Profile Page</h1>
+      <div className='profile-controls grid-container auto'>
+        {profileControls.map(({ id, controlName, linkTo, iconName }) => {
+          return (
+            <Link to={linkTo} key={id} className='profile-control'>
+              <span className='material-icons'>{iconName}</span>
+              <span className='control-name'>{controlName}</span>
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
