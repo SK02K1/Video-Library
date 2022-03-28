77ebd315d1d4e53cb93d39e267d7d80952cace7d
SK02K1 <ksourabh458@gmail.com>
2022-03-28 22:05:08 +0530

    add: user profile card component

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,10 +1,26 @@
 import { profileControls } from '../../utils';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../contexts';
 import './Profile.css';
 
 export const Profile = () => {
+  const {
+    userData: { firstName, lastName, email },
+  } = useAuth();
+
   return (
     <div className='content'>
+      <div className='user-profile-card m-md-b'>
+        <img className='user-avatar' src='/assets/avatar.svg' alt='avatar' />
+        <div className='user-info m-lg-l'>
+          <div className='username'>
+            <span className='firstname m-xs-r'>{firstName}</span>
+            <span className='lastname'>{lastName}</span>
+          </div>
+          <div className='email m-xs-t'>{email}</div>
+          <button className='btn btn-danger m-sm-t'>Logout</button>
+        </div>
+      </div>
       <div className='profile-controls grid-container auto'>
         {profileControls.map(({ id, controlName, linkTo, iconName }) => {
           return (
