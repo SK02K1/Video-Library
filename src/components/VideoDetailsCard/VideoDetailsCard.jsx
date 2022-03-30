451154d8542ce1ff2996d97713489f25e48dad1f
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2022-03-30 19:22:45 +0530

    refactor: remove unused imports

--- src/components/VideoDetailsCard/VideoDetailsCard.jsx
+++ src/components/VideoDetailsCard/VideoDetailsCard.jsx
@@ -1,4 +1,4 @@
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './VideoDetailsCard.css';
 
 export const VideoDetailsCard = ({ videoDetails }) => {
