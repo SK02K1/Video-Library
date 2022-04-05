15125b732bce97538aab684b6f33ac05ddd26361
SK02K1 <ksourabh458@gmail.com>
2022-04-05 23:21:06 +0530

    feat: create new playlist from playlists page itself

--- src/pages/Playlists/Playlists.jsx
+++ src/pages/Playlists/Playlists.jsx
@@ -1,18 +1,23 @@
+import './Playlists.css';
 import { useVideosData } from '../../contexts/videosData-context';
 import { PlaylistCard } from '../../components';
 import { useDocumentTitle } from '../../hooks';
+import { usePlaylistModal } from '../../contexts';
 
 export const Playlists = () => {
   useDocumentTitle('Playlists');
+  const { togglePlaylistModalState } = usePlaylistModal();
   const {
     videosDataState: { playlists },
   } = useVideosData();
   return (
     <div className='content'>
-      <h1 className='text-center text-xl m-xs-tb'>Playlists</h1>
-      <p className='text-center m-xs-tb'>
-        You have created {playlists.length} playlist
-      </p>
+      <header className='playlists-header'>
+        <h1 className='text-xl'>Playlists ({playlists.length})</h1>
+        <button onClick={togglePlaylistModalState} className='btn btn-primary'>
+          Create
+        </button>
+      </header>
       <div className='grid-container auto m-md-tb'>
         {playlists.map((playlist) => (
           <PlaylistCard key={playlist._id} playlist={playlist} />
